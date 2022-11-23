04a7a7328dce6aa4e7196b49925d91de290a37a8
Leibo <ido.leibole@gmail.com>
2022-11-23 17:07:12 +0200

    Get user and loader on state change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Login } from "./components/Login/Login";
@@ -8,14 +8,39 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./firebase";
+import { CircularProgress } from "@mui/material";
 
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState<any>(null);
+  const [loadingAuth, setLoadingAuth] = useState(true);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (newUser) => {
+      setLoadingAuth(false);
+      if (newUser) {
+        setUser(newUser);
+      } else {
+        setUser(null);
+      }
+    });
+  });
 
   return (
     <div className="App">
-      {user ? (
-        <div>application</div>
+      {loadingAuth ? (
+        <CircularProgress />
+      ) : user ? (
+        <div>
+          <button
+            onClick={() => {
+              signOut(auth);
+            }}
+          >
+            sign out
+          </button>
+        </div>
       ) : (
         <div>
           <Login setUser={setUser} />
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -10,7 +10,7 @@ import {
   signInWithPhoneNumber,
 } from "firebase/auth";
 import "firebase/compat/auth";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import { toast } from "react-toastify";
 
 declare global {
@@ -28,8 +28,11 @@ enum STEPS {
 
 export const Login = ({ setUser }: { setUser: (user: any) => void }) => {
   const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState("");
   const [step, setStep] = useState(STEPS.PHONE);
   const [verifying, setVerifying] = useState(false);
+
+  const confirmationResult = useRef<any>(null);
 
   const initiateLogin = async (e: FormEvent) => {
     e.preventDefault();
@@ -41,8 +44,9 @@ export const Login = ({ setUser }: { setUser: (user: any) => void }) => {
           setVerifying(true);
           console.log(response);
           signInWithPhoneNumber(auth, phone, window.recaptchaVerifier)
-            .then((confirmationResult) => {
-              console.log(confirmationResult);
+            .then((newConfirmationResult) => {
+              confirmationResult.current = newConfirmationResult;
+              setStep(STEPS.OTP);
             })
             .catch((error) => {
               console.error(error);
@@ -57,20 +61,41 @@ export const Login = ({ setUser }: { setUser: (user: any) => void }) => {
     window.captchaId = await window.recaptchaVerifier.verify();
   };
 
+  const submitOtp = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const codeResult = await confirmationResult.current.confirm(otp);
+      const user = codeResult.user;
+      setUser(user);
+    } catch (e) {
+      console.error(e);
+      toast.error("Wrong code");
+    }
+  };
+
   return (
     <Box>
-      <form onSubmit={initiateLogin}>
-        <Input value={phone} onChange={(e) => setPhone(e.target.value)} />
-        <Box marginTop={2}>
-          {verifying ? (
-            <CircularProgress />
-          ) : (
-            <button type="submit" id="sign-in-button">
-              Login
-            </button>
-          )}
-        </Box>
-      </form>
+      {step === STEPS.PHONE ? (
+        <form onSubmit={initiateLogin}>
+          <Input value={phone} onChange={(e) => setPhone(e.target.value)} />
+          <Box marginTop={2}>
+            {verifying ? (
+              <CircularProgress />
+            ) : (
+              <button type="submit" id="sign-in-button">
+                Login
+              </button>
+            )}
+          </Box>
+        </form>
+      ) : (
+        <form onSubmit={submitOtp}>
+          <Input value={otp} onChange={(e) => setOtp(e.target.value)} />
+          <Box marginTop={2}>
+            <button type="submit">Submit code</button>
+          </Box>
+        </form>
+      )}
     </Box>
   );
 };
